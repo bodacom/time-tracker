1722c9e44125c831bc4b9b810a0864a5f1374cc2
bodacom <pischanuk.oik@gmail.com>
2023-01-23 23:04:54 +0200

    Refactor script code
    Redundant code deleted. Naming and structure refactored.
    Saving to file transformen into function.
    Measurement accuracy added.
    Buffer processing implemented for cases of file inavailability.

--- main.py
+++ main.py
@@ -4,27 +4,42 @@ import re
 import sys
 from datetime import datetime
 
+
+
+def save_to_file(file: str, data: str):
+    try:
+        with open(file, 'a') as data_file:
+            data_file.write(data)
+        return True
+    except:
+        print('Eror writing to file. Data will be collected in buffer and written while write operation will be available.')
+        return False
+
+
 # getwindowfocus == getactivewindow
 # 'xdotool getwindowfocus getwindowname getactivewindow getmouselocation'
 
-xdo_command = 'xdotool getactivewindow getwindowname getmouselocation '
+XDO_COMMAND = 'xdotool getactivewindow getwindowname getmouselocation '
 data = ''
-file_name = 'tracker_log.txt'
+FILE_NAME = 'tracker_log_1.txt'
+MEASUREMENT_ACCURACY = 2 # measurement accuracy in seconds
+measurement_delay = MEASUREMENT_ACCURACY / 2
 
-# for i in range(10):
 while True:
 
-    xdo = os.popen(xdo_command).read()
-    now = datetime.now().time()
-    time_ = time.time()
-    # print(xdo_command, xdo, now, sep='\n', end='\n\n')
-    data = data + str(time_)+ '\n' + xdo + '\n'
-    
-    try:
-        with open(file_name, 'a') as data_file:
-            data_file.write(str(time_) + '\n' + xdo + '\n')
-        data = ''
-    except:
-        print('Eror writing to file. Data will be collected in buffer and written while write operation will be available.')
+    # get activity details and time stamp
+    activity_details = os.popen(XDO_COMMAND).read()
+    time_stamp = time.time()
 
-    time.sleep(1)
+    # form an activity frame
+    activity_frame = f'{str(time_stamp)}\n{activity_details}\n' #str(time_stamp)+ '\n' + activity_details + '\n'
+    
+    # form a data buffer variable
+    data = data + activity_frame
+    
+    # save current data to file. Clean buffer is saved to file successfuly
+    if save_to_file(FILE_NAME, data):
+        data = ''
+
+    # add delay to save resources with acceptable accuracy
+    time.sleep(measurement_delay)
