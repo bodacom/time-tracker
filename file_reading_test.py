1cdbfebe2d605f0d55c368b715fbe551a03936fa
bodacom <pischanuk.oik@gmail.com>
2023-01-29 19:05:09 +0200

    Ass configuration for seek testing
    Defined functions and configuration for seek mode testing.
    (Does not work for text files)

--- file_reading_test.py
+++ file_reading_test.py
@@ -27,6 +27,31 @@ def check_log(log_file_name: str = 'tracker_log.txt') -> list:
         print(previous_line)
 
 
+def seek_log(log_file_name: str = 'tracker_log.txt') -> list:
+    '''
+    Seek to the end of file and back to already tracked lines.
+    Returns nothing so far.
+    
+    File format.
+    One entity consists of three lines like the next:
+
+     "1665761850.8328192                            (contains unix time stamp)
+      main.py - beetroot - Visual Studio Code       (contains the window and the task name)
+      x:1191 y:1051 screen:0 window:115343363"      (contains coursore coordinates x and y, screen number, window id)
+                                                    (contains empty line as an entity separator)
+      -- // --
+    '''
+    
+    with open(log_file_name, 'r') as log_file:
+        
+        log_file.seek(0,2)
+        print(type(log_file.tell()), log_file.tell())
+        # for i in range(100):
+        #     letter = log_file.read(1)
+        #     log_file.seek(-2,1)
+        #     print(letter, sep='', end='')
+
+
 def read_all_file(repeat_times, sleep_time):
     for i in range(repeat_times):
         start_time = time.time()
@@ -60,9 +85,20 @@ def check_through_file(repeat_times, sleep_time):
         time.sleep(sleep_time)
 
 
+def seek_file(repeat_times, sleep_time):
+    for i in range(repeat_times):
+        start_time = time.time()
+        seek_log("/home/bohdan/tracker_log.txt")
+        finish_time = time.time()
+
+        print('Read time = ', finish_time - start_time)
+        #print('Number of lines = ', lines_quantity)
+
+        time.sleep(sleep_time)
+
 if __name__ == '__main__':
 
     # read_all_file(1,0)
 
-    check_through_file(3,1)
+    seek_file(3,1)
 
