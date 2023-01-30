fa4aca52c1a7c2b5b94c0fd0773a06fbc15254db
bodacom <pischanuk.oik@gmail.com>
2023-01-30 13:56:14 +0200

    Refactor widget
    Widget refactor to less code without use of ANSI escape sequence

--- widget.py
+++ widget.py
@@ -2,12 +2,7 @@ import os
 import time
 
 
-
-first = True
 while True:
-    if first:
-        print()
-        first = False
-    print(os.popen('xdotool getactivewindow getwindowname').read(), end='')
+    os.system('clear')
+    print('\n', os.popen('xdotool getactivewindow getwindowname').read(), end='')
     time.sleep(1)
-    print('\033[1A', end='\x1b[2K')
