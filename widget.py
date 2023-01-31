3d688b51436dfaf2c107ebee84474226335fe61b
bodacom <pischanuk.oik@gmail.com>
2023-01-31 16:26:00 +0200

    Add list of tracked activities to GUI
    List of activities added to GUI.

--- widget.py
+++ widget.py
@@ -6,16 +6,18 @@ import tkinter as tk
 
 def main():
 
-    counter = 1
+    counter = 0
     hours = 0
     minutes = 0
     seconds = 0
     previous_activity = ''
     session_activities = {}
     first_time = True
-    text_to_print = ''
+    text_to_print = '00:00:00'
     text_display = canvas.create_text(20, 20, text=text_to_print, font=('Helvetica',14),\
                            fill='green',state='hidden', anchor=tk.NW)
+    list_to_display = ''
+    list_display = None
 
     while True:
         
@@ -50,15 +52,20 @@ def main():
 
             i = 0
             a = dict(sorted(session_activities.items(), key=lambda item: item[1], reverse=True))
+            list_to_display = ''
             for activity, activity_time in a.items():
                 seconds = activity_time % 60
                 minutes = activity_time // 60
                 hours = minutes // 60
                 minutes = minutes % 60
-                print(f' {hours:02}:{minutes:02}:{seconds:02}  ', activity)
+                list_to_display = list_to_display + f'{hours:02}:{minutes:02}:{seconds:02}   {activity}\n'
                 i += 1
                 if i == 9:
                     break
+            canvas.itemconfig(list_display, state='hidden')
+            del(list_display)
+            list_display = canvas.create_text(20, 60, text=list_to_display, font=('Helvetica',14),\
+                           fill='black',state='normal', anchor=tk.NW)
             i = 0
         else:
             first_time = False
@@ -74,7 +81,7 @@ if __name__ == '__main__':
     window.title("Tracker widget")
     window.resizable(1, 1)
     window.wm_attributes("-topmost",1)
-    canvas = tk.Canvas(window, width=700, height=200, bd=0, highlightthickness=0)
+    canvas = tk.Canvas(window, width=700, height=400, bd=0, highlightthickness=1)
     canvas.pack()
     window.update()
 
